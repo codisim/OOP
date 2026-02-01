96b2fd269d990f2bfea8d419a105a1da4caeefc3
MD Waliullah <waliullah.godev@gmail.com>
2026-02-01 16:01:44 +0600

    Add Employee class extending UniPerson; implement private properties for employeeID and salary with appropriate getters and setters.

---
+++ project/person/Employee.js
@@ -0,0 +1,36 @@
+
+
+const _employeeID = Symbol("_employeeID");
+const salary = Symbol("salary");
+
+
+
+class Employee extends UniPerson {
+    constructor(id, name, employeeID){
+        super(id, name);
+        this[_employeeID] = employeeID;
+        this[salary] = null;
+    }
+
+    get employeeID(){
+        return this[_employeeID];
+    }
+
+ 
+   
+    get salary(){
+        return this[salary];
+    }
+
+
+    set salary(amount){
+        this[salary] = amount;
+    }
+
+    toString(){
+        return `${super.toString()}, employeeID: ${this[_employeeID]}, Salary: ${this[salary]}`;
+    }
+}
+
+
+module.exports = Employee;
