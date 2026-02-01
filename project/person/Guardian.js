fb21922c8cfab3ff95ce11b418a1b5bddb69c417
MD Waliullah <waliullah.godev@gmail.com>
2026-02-01 15:58:35 +0600

    Add Guardian and Student classes with private properties using Symbols; implement methods for profession, income, studentID, guardian, exams, and fees management.

--- project/person/Guardian.js
+++ project/person/Guardian.js
@@ -1,1 +1,33 @@
 
+const _profession = Symbol("profession");
+const _income = Symbol("income");
+
+class Guardian extends Person {
+    constructor(id, name) {
+        super(id, name);
+        this[_profession] = null;
+        this[_income] = null;
+    }
+
+    get profession() {
+        return this[_profession];
+    }
+
+    set profession(profession) {
+        this[_profession] = profession;
+    }
+
+    get income() {
+        return this[_income];
+    }
+
+    set income(income) {
+        this[_income] = income;
+    }
+
+    toString() {
+        return `${super.toString()}, Profession: ${this[_profession]}, Income: ${this[_income]}`;
+    }
+}
+
+export default Guardian;
---
+++ project/person/Student.js
@@ -0,0 +1,53 @@
+
+
+const _studentID = Symbol("studentID");
+const _guardian = Symbol("guardian");
+const _exams = Symbol("exams");
+const _fees = Symbol("fees");
+
+
+
+class Student extends UniPerson {
+    constructor(id, name, studentID, guardian){
+        super(id, name);
+        this[_studentID] = studentID;
+        this[_guardian] = guardian;
+        this[_exams] = [];
+        this[_fees] = null;
+    }
+
+    get studentID(){
+        return this[_studentID];
+    }
+
+    get guardian(){
+        return this[_guardian];
+    }
+
+    set guardian(guardian){
+        this[_guardian] = guardian;
+    }
+
+    get exams(){
+        return this[_exams];
+    }
+
+    addExam(exam){
+        this[_exams].push(exam);
+    }
+
+    get fees(){
+        return this[_fees];
+    }
+
+    payFees(amount){
+        this[_fees] += amount;
+    }
+
+    toString(){
+        return `${super.toString()}, StudentID: ${this[_studentID]}, Exams: ${this[_exams]}, Fees Paid: ${this[_fees]}`;
+    }
+}
+
+
+export default Student;
