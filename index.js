02352d2b0a4ba5ead1cdf4e820edfdc14b4ae579
MD Waliullah <waliullah.godev@gmail.com>
2026-02-01 11:25:48 +0600

    methods

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ class Person {
         this.name = name;
         this.email = email;
     }
+
+
+    changeName(name){
+        this.name = name;
+    }
+
+    sendEmail(msg){
+        console.log('To', this.email);
+        console.log('Sending Email');
+        console.log('MSG: ', msg);
+    }
+
+
 }
 
 
 const p1 = new Person("Habiba", "habiba@gmail.com");
 const p2 = new Person("Farhana", "farhana@gmail.com");
 
+
+
+p1.sendEmail("Hello Habiba, How are you?");
+p2.sendEmail("Hello Farhana, How have you been?");
 
 console.log(p1);
 console.log(p2);
