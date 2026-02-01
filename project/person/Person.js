a4e459b304f4bc44b24739817e71d26fc30873f8
MD Waliullah <waliullah.godev@gmail.com>
2026-02-01 15:41:22 +0600

    Implement Person class with private properties using Symbols; add getters and setters for id, name, blood, and contact.

---
+++ project/person/Person.js
@@ -0,0 +1,50 @@
+
+
+const _id = Symbol('id');
+const _name = Symbol('name');
+const _blood = Symbol('blood');
+const _contact = Symbol('contact');
+
+
+
+class Person {
+    constructor(id, name){
+        this[_id] = id;
+        this[_name] = name;
+        this[_blood] = null;
+        this[_contact] = null;
+    }
+
+    get id(){
+        return this[_id];
+    }
+
+    get name(){
+        return this[_name];
+    }
+
+    set name(name){
+        this[_name] = name;
+    }
+
+    get blood(){
+        return this[_blood];
+    }
+
+    set blood(blood){
+        this[_blood] = blood;
+    }
+
+    get contact(){
+        return this[_contact];
+    }
+
+    set contact(contact){
+        this[_contact] = contact;
+    }
+
+    toString(){
+        return `Person [ID: ${this[_id]}, Name: ${this[_name]}, Blood: ${this[_blood]}, Contact: ${this[_contact]}]`;
+    }
+
+}
